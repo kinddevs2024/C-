00e7b44eb0c39b365251689537809cc5293a1319
kinddevs <kinddevs2024@gmail.com>
2024-11-28 22:03:30 +0500

    begin 1-9

--- begin/index.cpp
+++ begin/index.cpp
@@ -14,7 +14,7 @@ int main()
 
     float x = pow(3.0 / 4.0, sqrt(pow(4, 3.0 / 2.0)));
     cout << "\nikkinchi savol javobi : " << x;
-
+ 
     // uchinchi savol
     float m = 1.23;
     float y = pow(10, cos(m)) * pow(2, 12);
