28d6f90b5a842a5e5a5169beea01adbb9c464415
umarbek <kinddevs2024@gmail.com>
2024-12-06 11:40:05 +0500

    finish of begin and integer

---
+++ C--main/integer/integer15/integer15.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int son;
+
+    cout << "Uch xonali son kiriting: ";
+    cin >> son;
+
+    //ishlash
+    int yuzlik = son / 100;  
+    int onlik = (son / 10) % 10;  
+    int birlik = son % 10;          
+
+    cout << "Raqamlar teskarisi: " << onlik  << yuzlik << birlik << endl;
+
+      int yangi_son = birlik*100 + onlik * 1 + yuzlik*10;
+        cout << "Raqamlar teskarisi: " << yangi_son << endl;
+  
+    return 0;
+}
